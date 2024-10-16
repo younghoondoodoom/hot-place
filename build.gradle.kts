d7a51c4f34b41854e00d54c9b106272637fb02a0
younghooncazzle <younghoon.choi@lotte.net>
2024-10-16 10:55:42 +0900

    initial config

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,6 +33,11 @@ dependencies {
     testImplementation("io.projectreactor:reactor-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    implementation("org.springdoc:springdoc-openapi-starter-webflux-ui:2.1.0")
+    implementation("org.springdoc:springdoc-openapi-starter-webflux-api:2.1.0")
+    implementation("org.springdoc:springdoc-openapi-starter-common:2.1.0")
+    implementation("org.springdoc:springdoc-openapi-kotlin:1.7.0")
 }
 
 kotlin {
